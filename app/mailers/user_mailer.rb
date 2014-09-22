3d59416cda30b71503c1c7c4d635aa441c297ad2
Corey <burkleycorey@yahoo.com>
2014-09-22 15:20:24 -0500

    New Email Functionality
    Implemented new user functionality with UserMailer
    Now able to send 'Thank you' email to the new user once successful.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,5 @@ class UserMailer < ActionMailer::Base
   def registration_confirmation(user)
     @user = user
     mail(:to => user.email, :subject => "Registered")
+  end
 end
--- config/initializers/steup_email.rb
+++ config/initializers/steup_email.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.gmail.com',
-  :port                 => '587',
+  :port                 => 587,
   :enable_starttls_auto => true,
-  :user_name            => 'thisistheotherguy',
+  :user_name            => 'thisistheotherguy@gmail.com',
   :password             => 'thisisatestforruby',
   :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
   :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
